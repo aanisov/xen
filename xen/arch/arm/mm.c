5daa27fe9095e46b9efed2a5dc80568b1943a54d
Benjamin Sanda <ben.sanda () dornerworks ! com>
2016-04-04 18:48:44 +0300

    xentrace: Memory/Page Mapping support for DOMID_XEN on ARM
    Modified ARM arch specific p2m.c and mm.c files to provide support for
    xentrace. Case for DOMID_XEN added xenmem_add_to_physmap_one() when
    XENMAPSPACE_gmfn_foreign domain requested via get_pg_owner. This
    provides correct calls to rcu_lock_domain() when DOMID_XEN is
    requested.
    
    Check for DOMID_XEN added to p2m_lookup() which skips PFN to MFN
    translation. DOMID_XEN is considered a PV guest on x86 (i.e MFN ==
    GFN), but on ARM there is no such concept. Thus requests to DOMID_XEN
    on ARM use a MFN address directly and do not need translation from
    PFN.
    
    Check added to p2m_lookup() to determine read/write or read-only page
    type when requesting DOMID_XEN. This is  done by checking the
    u.inuse.type_info parameter of the requested page. The page rw/ro
    paddr_t type is then set accordingly.

--- xen/arch/arm/mm.c
+++ xen/arch/arm/mm.c
@@ -1099,7 +1099,8 @@ int xenmem_add_to_physmap_one(
     {
         struct domain *od;
         p2m_type_t p2mt;
-        od = rcu_lock_domain_by_any_id(foreign_domid);
+        od = get_pg_owner(foreign_domid);
+
         if ( od == NULL )
             return -ESRCH;
 
