2feb6ef1fc464cd2b902b865af9bc0360c2fd864
Benjamin Sanda <ben.sanda () dornerworks ! com>
2016-04-04 18:48:43 +0300

    xentrace: Common Support for get_pg_owner/put_pg_owner on ARM and x86
    Moved get_pg_owner() and put_pg_owner() from the arch specific x86
    mm.c source files into the common page_alloc.c source. This was done
    as theses functions are now needed by both architectures to support
    xentrace on the ARM platform. Forward declarations were added to mm.h.
    
    One conditional compilation check was added in get_pg_owner() for the
    is_pvh_domain(curr) check which is only valid to perform on x86
    platforms.
    
    Signed-off-by: Benjamin Sanda <ben.sanda@dornerworks.com>

--- xen/include/xen/mm.h
+++ xen/include/xen/mm.h
@@ -576,6 +576,9 @@ int xenmem_add_to_physmap_one(struct domain *d, unsigned int space,
 int xenmem_add_to_physmap(struct domain *d, struct xen_add_to_physmap *xatp,
                           unsigned int start);
 
+struct domain *get_pg_owner(domid_t domid);
+void put_pg_owner(struct domain *pg_owner);
+
 /* Return 0 on success, or negative on error. */
 int __must_check guest_remove_page(struct domain *d, unsigned long gmfn);
 int __must_check steal_page(struct domain *d, struct page_info *page,
