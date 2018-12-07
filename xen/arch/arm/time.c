64f71af4a810c8b630897e2dd9b60c206914b878
Andrii Anisov <andrii_anisov@epam.com>
2018-12-07 20:09:15 +0200

    4tbm:arch/arm: Route phys timer IRQ to guest domains
    Based on Stefano's patch shown here [1]
    
    [1] https://marc.info/?l=xen-devel&m=148668817704668
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/arch/arm/time.c
+++ xen/arch/arm/time.c
@@ -296,10 +296,9 @@ void init_timer_interrupt(void)
 {
     /* Sensible defaults */
     WRITE_SYSREG64(0, CNTVOFF_EL2);     /* No VM-specific offset */
-    /* Do not let the VMs program the physical timer, only read the physical counter */
-    WRITE_SYSREG32(CNTHCTL_EL2_EL1PCTEN, CNTHCTL_EL2);
     WRITE_SYSREG32(0, CNTP_CTL_EL0);    /* Physical timer disabled */
     WRITE_SYSREG32(0, CNTHP_CTL_EL2);   /* Hypervisor's timer disabled */
+    WRITE_SYSREG32(CNTHCTL_EL2_EL1PCTEN|CNTHCTL_EL2_EL1PCEN, CNTHCTL_EL2);
     isb();
 
     request_irq(timer_irq[TIMER_HYP_PPI], 0, timer_interrupt,
