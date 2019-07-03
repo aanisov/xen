d42939b6fb67a90f6c7044288a4953400f23bc21
Andrii Anisov <andrii_anisov@epam.com>
2019-07-03 20:03:17 +0300

    rename schedule_tail() to ctxt_switch_tail()
    The function is not schedule related, but does context switch
    job. While we would need schedule_tail name soon so does name
    the function appropriately.
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/arch/arm/domain.c
+++ xen/arch/arm/domain.c
@@ -320,7 +320,7 @@ void update_runstate_area(struct vcpu *v)
     }
 }
 
-static void schedule_tail(struct vcpu *prev)
+static void ctxt_switch_tail(struct vcpu *prev)
 {
     ctxt_switch_from(prev);
 
@@ -342,7 +342,7 @@ static void continue_new_vcpu(struct vcpu *prev)
     current->arch.actlr = READ_SYSREG32(ACTLR_EL1);
     processor_vcpu_initialise(current);
 
-    schedule_tail(prev);
+    ctxt_switch_tail(prev);
 
     if ( is_idle_vcpu(current) )
         reset_stack_and_jump(idle_loop);
@@ -380,7 +380,7 @@ void context_switch(struct vcpu *prev, struct vcpu *next)
 
     prev = __context_switch(prev, next);
 
-    schedule_tail(prev);
+    ctxt_switch_tail(prev);
 }
 
 void continue_running(struct vcpu *same)
