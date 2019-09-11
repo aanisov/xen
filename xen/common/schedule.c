0a2f5de163de4b21129146bf5e31e30bd188c534
Andrii Anisov <andrii_anisov@epam.com>
2019-09-11 12:33:48 +0300

    tacc: Introduce a lockless interface for guest time
    The lockless interface to acquire guest time by scheduling code
    is introduced. It can be used by schedulers what do not require
    guest time from a different pcpu to take scheduling decission.
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/common/schedule.c
+++ xen/common/schedule.c
@@ -2395,6 +2395,16 @@ void tacc_irq_exit(int place)
     tacc->irq_cnt--;
 }
 
+s_time_t tacc_get_guest_time(struct tacc *tacc)
+{
+    s_time_t guest_time;
+
+    guest_time = tacc->state_time[TACC_GUEST];
+    guest_time += tacc->state_time[TACC_GSYNC];
+
+    return guest_time;
+}
+
 static void sched_slave(void)
 {
     struct vcpu          *v, *vprev = current;
